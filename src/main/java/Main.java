725a0f07eae50664891828c46182f8e9022039f1
Wilfred Dube <wilfreddube@gmail.com>
2023-01-15 11:52:08 +0200

    Clean up code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -39,7 +39,6 @@ public class Main {
             i++;
         }
 
-        System.out.println("========================================================");
         System.out.println(carPark);
         System.out.println("========================================================");
     }
